8d7fed8e77265b34147b56de5e7040bb11b08a19
Joel Wallis Jucá <joelwallis@gmail.com>
2018-01-13 18:21:48 -0300

    mix new splitshitbot

---
+++ lib/splitshitbot.ex
@@ -0,0 +1,18 @@
+defmodule Splitshitbot do
+  @moduledoc """
+  Documentation for Splitshitbot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Splitshitbot.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/splitshitbot_test.exs
@@ -0,0 +1,8 @@
+defmodule SplitshitbotTest do
+  use ExUnit.Case
+  doctest Splitshitbot
+
+  test "greets the world" do
+    assert Splitshitbot.hello() == :world
+  end
+end
